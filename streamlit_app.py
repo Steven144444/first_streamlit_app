cacffec1a68da7bb6dcd785a76272d3c161f8dc2
MENSAH Steeve <74554422+Steven144444@users.noreply.github.com>
2022-10-14 16:48:32 +0200

    Update streamlit_app.py
    added pandas library

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,12 @@
-import streamlit
-streamlit.title("My Parents New Healthy Diner")
+import streamlit as st
+st.title("My Mom's New Healthy Diner")
 
-streamlit.header("Breakfast Menu")
-streamlit.text("Omega 3 & Blueberry Oatmeal")
-streamlit.text("Kale, Spinach & Rocket Smoothie")
-streamlit.text("Hard Boiled Free-Range Egg")
+st.header(" Breakfast Favorites")
+st.text("Omega 3 & Blueberry Oatmeal")
+st.text("Kale, Spinach & Rocket Smoothie")
+st.text("Hard Boiled Free-Range Egg")
+st.text("Avocado Toast")
+
+import pandas as pd
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+st.dataframe(my_fruit_list)
