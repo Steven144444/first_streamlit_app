44a5aad65296a2cf52938a4a32791aa8aae7d5eb
MENSAH Steeve <74554422+Steven144444@users.noreply.github.com>
2022-10-17 09:41:08 +0200

    Update streamlit_app.py
    choose the fruit name column as the index

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,8 @@ st.text("Avocado Toast")
 
 import pandas as pd
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
 st.header("Build Your Own Fruit Smoothie")
 
 # Let's put a pick list here so they can pick the fruit they want to include 
