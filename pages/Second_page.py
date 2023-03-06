05ec7be5e72166c0615a26120dde7b805b239f7c
MENSAH Steeve <74554422+Steven144444@users.noreply.github.com>
2023-03-06 16:57:08 +0100

    Update Second_page.py

--- pages/Second_page.py
+++ pages/Second_page.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
 if st.button('Click to run the FIRST MAIN JOB'):
-  pass
+  http://51.103.32.188/rest/v1/group/name/smensah/project/name/yelp/version/name/default/job/name/1bis-New_data_ingestion/run?environmentName=dev
