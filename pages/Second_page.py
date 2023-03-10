4269f23ed7cfefa46fe1319e37ce9c7ef474478c
MENSAH Steeve <74554422+Steven144444@users.noreply.github.com>
2023-03-10 11:24:07 +0100

    Update Second_page.py

--- pages/Second_page.py
+++ pages/Second_page.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import requests
+import webbrowser
 
 
 if st.button('Click to run the FIRST MAIN JOB'):
-  requests.post("http://51.103.32.188/rest/v1/group/name/smensah/project/name/yelp/version/name/default/job/name/1bis-New_data_ingestion/run?environmentName=dev")
+  #requests.post("http://51.103.32.188/rest/v1/group/name/smensah/project/name/yelp/version/name/default/job/name/1bis-New_data_ingestion/run?environmentName=dev")
+  webbrowser.open_new_tab('http://51.103.32.188/rest/v1/group/name/smensah/project/name/yelp/version/name/default/job/name/1bis-New_data_ingestion/run?environmentName=dev')
