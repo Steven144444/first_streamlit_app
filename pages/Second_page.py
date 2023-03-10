d93d96a9e33f655d1b8197f841b83eb9b5902e41
MENSAH Steeve <74554422+Steven144444@users.noreply.github.com>
2023-03-10 11:56:25 +0100

    Update Second_page.py

--- pages/Second_page.py
+++ pages/Second_page.py
@@ -2,18 +2,23 @@ import streamlit as st
 import requests
 import webbrowser
 import subprocess
+import shlex
 
 matillion = 'curl -X POST -u azure-user:azure-user -k http://51.103.32.188/rest/v1/group/name/smensah/project/name/yelp/version/name/default/job/name/1bis-New_data_ingestion/run?environmentName=dev'
+cmd = '''curl -X POST -u azure-user:azure-user -k http://51.103.32.188/rest/v1/group/name/smensah/project/name/yelp/version/name/default/job/name/1bis-New_data_ingestion/run?environmentName=dev'''
 
 if st.button('Click to run the FIRST MAIN JOB'):
   #requests.post("http://51.103.32.188/rest/v1/group/name/smensah/project/name/yelp/version/name/default/job/name/1bis-New_data_ingestion/run?environmentName=dev")
   #webbrowser.open_new_tab('http://51.103.32.188/rest/v1/group/name/smensah/project/name/yelp/version/name/default/job/name/1bis-New_data_ingestion/run?environmentName=dev')
-  subprocess.call([
-    'curl',
-    '-X',
-    'POST',
-    '-u',
-    'azure-user:azure-user',
-    '-k',
-    'http://51.103.32.188/rest/v1/group/name/smensah/project/name/yelp/version/name/default/job/name/1bis-New_data_ingestion/run?environmentName=dev'
-    ])
+#   subprocess.call([
+#     'curl',
+#     '-X',
+#     'POST',
+#     '-u',
+#     'azure-user:azure-user',
+#     '-k',
+#     'http://51.103.32.188/rest/v1/group/name/smensah/project/name/yelp/version/name/default/job/name/1bis-New_data_ingestion/run?environmentName=dev'
+#     ])
+  args = shlex.split(cmd)
+  process = subprocess.Popen(args, shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+  stdout, stderr = process.communicate()
