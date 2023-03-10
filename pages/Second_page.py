f15a49b76f3eb6a89d162c616ebf5d945a726928
MENSAH Steeve <74554422+Steven144444@users.noreply.github.com>
2023-03-10 11:46:00 +0100

    Update Second_page.py

--- pages/Second_page.py
+++ pages/Second_page.py
@@ -9,4 +9,4 @@ if st.button('Click to run the FIRST MAIN JOB'):
   #requests.post("http://51.103.32.188/rest/v1/group/name/smensah/project/name/yelp/version/name/default/job/name/1bis-New_data_ingestion/run?environmentName=dev")
   #webbrowser.open_new_tab('http://51.103.32.188/rest/v1/group/name/smensah/project/name/yelp/version/name/default/job/name/1bis-New_data_ingestion/run?environmentName=dev')
   subprocess.Popen(matillion)
-  subprocess.check_output(['bash','-c', matillion])
+  output = subprocess.check_output(['bash','-c', matillion])
