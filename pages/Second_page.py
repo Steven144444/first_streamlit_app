8c0e4d39fd896999c3c2799b3ace52cbb629c1a5
MENSAH Steeve <74554422+Steven144444@users.noreply.github.com>
2023-03-10 11:42:35 +0100

    Update Second_page.py

--- pages/Second_page.py
+++ pages/Second_page.py
@@ -1,8 +1,12 @@
 import streamlit as st
 import requests
 import webbrowser
+import subprocess
 
+matillion = 'curl -X POST -u azure-user:azure-user -k http://51.103.32.188/rest/v1/group/name/smensah/project/name/yelp/version/name/default/job/name/1bis-New_data_ingestion/run?environmentName=dev'
 
 if st.button('Click to run the FIRST MAIN JOB'):
   #requests.post("http://51.103.32.188/rest/v1/group/name/smensah/project/name/yelp/version/name/default/job/name/1bis-New_data_ingestion/run?environmentName=dev")
-  webbrowser.open_new_tab('http://51.103.32.188/rest/v1/group/name/smensah/project/name/yelp/version/name/default/job/name/1bis-New_data_ingestion/run?environmentName=dev')
+  #webbrowser.open_new_tab('http://51.103.32.188/rest/v1/group/name/smensah/project/name/yelp/version/name/default/job/name/1bis-New_data_ingestion/run?environmentName=dev')
+  subprocess.Popen(matillion)
+  subprocess.check_output(['bash','-c', matillion])
