5907edc925bf85e93925af215d4cda1ad239b5ef
MENSAH Steeve <74554422+Steven144444@users.noreply.github.com>
2023-03-10 12:00:16 +0100

    Update Second_page.py

--- pages/Second_page.py
+++ pages/Second_page.py
@@ -20,5 +20,6 @@ if st.button('Click to run the FIRST MAIN JOB'):
 #     'http://51.103.32.188/rest/v1/group/name/smensah/project/name/yelp/version/name/default/job/name/1bis-New_data_ingestion/run?environmentName=dev'
 #     ])
   args = shlex.split(cmd)
-  process = subprocess.Popen(args, shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-  stdout, stderr = process.communicate()
+  #process=
+  subprocess.Popen(args, shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+  #stdout, stderr = process.communicate()
