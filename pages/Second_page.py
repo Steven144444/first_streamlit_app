e2a19483ce076b0a0162042b6002601e3735b96d
MENSAH Steeve <74554422+Steven144444@users.noreply.github.com>
2023-03-10 11:51:33 +0100

    Update Second_page.py

--- pages/Second_page.py
+++ pages/Second_page.py
@@ -8,5 +8,12 @@ matillion = 'curl -X POST -u azure-user:azure-user -k http://51.103.32.188/rest/
 if st.button('Click to run the FIRST MAIN JOB'):
   #requests.post("http://51.103.32.188/rest/v1/group/name/smensah/project/name/yelp/version/name/default/job/name/1bis-New_data_ingestion/run?environmentName=dev")
   #webbrowser.open_new_tab('http://51.103.32.188/rest/v1/group/name/smensah/project/name/yelp/version/name/default/job/name/1bis-New_data_ingestion/run?environmentName=dev')
-  subprocess.Popen(matillion)
-  output = subprocess.check_output(['bash','-c', matillion])
+  subprocess.call([
+    'curl',
+    '-X',
+    'POST',
+    '-u',
+    azure-user:azure-user,
+    '-k',
+    'http://51.103.32.188/rest/v1/group/name/smensah/project/name/yelp/version/name/default/job/name/1bis-New_data_ingestion/run?environmentName=dev'
+    ])
