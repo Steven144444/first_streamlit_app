77a3df2781b02ed8cdccda1180ce13ee8fed79d2
MENSAH Steeve <74554422+Steven144444@users.noreply.github.com>
2023-03-13 10:40:30 +0100

    Update Second_page.py

--- pages/Second_page.py
+++ pages/Second_page.py
@@ -9,8 +9,8 @@ matillion = 'curl -X POST -u azure-user:azure-user -k http://51.103.32.188/rest/
 cmd = '''curl -X POST -u azure-user:azure-user -k http://51.103.32.188/rest/v1/group/name/smensah/project/name/yelp/version/name/default/job/name/1bis-New_data_ingestion/run?environmentName=dev'''
 
 if st.button('Click to run the FIRST MAIN JOB'):
-  x = requests.get("http://51.103.32.188/rest/v1", auth = ('azure-user', 'azure-user'))
-  print(x)
+  x = requests.get('http://51.103.32.188/rest/v1', auth = ('azure-user', 'azure-user'))
+  st.text(x)
   
 #   subprocess.call([
 #     'curl',
