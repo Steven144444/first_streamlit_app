356d633b9a3d8169d4a7a1ae69d14fd49b628531
MENSAH Steeve <74554422+Steven144444@users.noreply.github.com>
2023-03-13 13:15:36 +0100

    Update Second_page.py

--- pages/Second_page.py
+++ pages/Second_page.py
@@ -8,7 +8,7 @@ matillion = 'curl -X POST -u azure-user:azure-user -k http://51.103.32.188/rest/
 cmd = '''curl -X POST -u azure-user:azure-user -k http://51.103.32.188/rest/v1/group/name/smensah/project/name/yelp/version/name/default/job/name/1bis-New_data_ingestion/run?environmentName=dev'''
 
 if st.button('Click to run the MAIN JOB'):
-  subprocess.run(curl -X POST -u azure-user:azure-user -k 'http://51.103.32.188/rest/v1/group/name/smensah/project/name/yelp/version/name/default/job/name/1bis-New_data_ingestion/run?environmentName=dev')
+  subprocess.run(curl -X POST -u azure-user:azure-user -k http://51.103.32.188/rest/v1/group/name/smensah/project/name/yelp/version/name/default/job/name/1bis-New_data_ingestion/run?environmentName=dev)
 #   subprocess.call([:
 #     'curl',
 #     '-X',
