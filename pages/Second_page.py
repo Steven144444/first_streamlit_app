4e5d33603704df5083308367a867acc510088f8b
MENSAH Steeve <74554422+Steven144444@users.noreply.github.com>
2023-03-13 10:10:13 +0100

    Update Second_page.py

--- pages/Second_page.py
+++ pages/Second_page.py
@@ -11,7 +11,7 @@ cmd = '''curl -X POST -u azure-user:azure-user -k http://51.103.32.188/rest/v1/g
 
 if st.button('Click to run the FIRST MAIN JOB'):
   #requests.post("http://51.103.32.188/rest/v1/group/name/smensah/project/name/yelp/version/name/default/job/name/1bis-New_data_ingestion/run?environmentName=dev")
-  webbrowser.open_new_tab('https://documentation.matillion.com/docs/2282996')
+  webbrowser.open_new_tab('https://documentation.matillion.com/docs/')
 #   subprocess.call([
 #     'curl',
 #     '-X',
