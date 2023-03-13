a89b68a77392a386f65838ad9d3082b936acb0f5
MENSAH Steeve <74554422+Steven144444@users.noreply.github.com>
2023-03-13 10:52:28 +0100

    Update Second_page.py

--- pages/Second_page.py
+++ pages/Second_page.py
@@ -8,8 +8,10 @@ st.title("Test curl command")
 matillion = 'curl -X POST -u azure-user:azure-user -k http://51.103.32.188/rest/v1/group/name/smensah/project/name/yelp/version/name/default/job/name/1bis-New_data_ingestion/run?environmentName=dev'
 cmd = '''curl -X POST -u azure-user:azure-user -k http://51.103.32.188/rest/v1/group/name/smensah/project/name/yelp/version/name/default/job/name/1bis-New_data_ingestion/run?environmentName=dev'''
 
+myobj = {'somekey': 'somevalue'}
+
 if st.button('Click to run the MAIN JOB'):
-  x = requests.post('http://51.103.32.188/rest/v1/group/name/smensah/project/name/yelp/version/name/default/job/name/1bis-New_data_ingestion/run?environmentName=dev', auth = ('azure-user', 'azure-user'))
+  x = requests.post('http://51.103.32.188/rest/v1/group/name/smensah/project/name/yelp/version/name/default/job/name/1bis-New_data_ingestion/run?environmentName=dev', data = myobj, auth = ('azure-user', 'azure-user'))
   st.text(x.text)
   
 #   subprocess.call([
