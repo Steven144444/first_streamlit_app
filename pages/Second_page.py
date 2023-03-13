2b0005b7fbcf9cb82ed003e0d00ac34aac7b283d
MENSAH Steeve <74554422+Steven144444@users.noreply.github.com>
2023-03-13 10:40:45 +0100

    Update Second_page.py

--- pages/Second_page.py
+++ pages/Second_page.py
@@ -8,7 +8,7 @@ st.title("Test curl command")
 matillion = 'curl -X POST -u azure-user:azure-user -k http://51.103.32.188/rest/v1/group/name/smensah/project/name/yelp/version/name/default/job/name/1bis-New_data_ingestion/run?environmentName=dev'
 cmd = '''curl -X POST -u azure-user:azure-user -k http://51.103.32.188/rest/v1/group/name/smensah/project/name/yelp/version/name/default/job/name/1bis-New_data_ingestion/run?environmentName=dev'''
 
-if st.button('Click to run the FIRST MAIN JOB'):
+if st.button('Click to run the MAIN JOB'):
   x = requests.get('http://51.103.32.188/rest/v1', auth = ('azure-user', 'azure-user'))
   st.text(x)
   
