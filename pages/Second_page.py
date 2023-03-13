9c051e838ddda765635d8dc8cb42be109ef21621
MENSAH Steeve <74554422+Steven144444@users.noreply.github.com>
2023-03-13 11:16:31 +0100

    Update Second_page.py

--- pages/Second_page.py
+++ pages/Second_page.py
@@ -1,5 +1,4 @@
 import streamlit as st
-import requests
 import subprocess
 import shlex
 
@@ -9,10 +8,7 @@ matillion = 'curl -X POST -u azure-user:azure-user -k http://51.103.32.188/rest/
 cmd = '''curl -X POST -u azure-user:azure-user -k http://51.103.32.188/rest/v1/group/name/smensah/project/name/yelp/version/name/default/job/name/1bis-New_data_ingestion/run?environmentName=dev'''
 
 if st.button('Click to run the MAIN JOB'):
-  x = requests.get('http://51.103.32.188/rest/v1/group/name/smensah/project/name/yelp/task/id/44442', auth = ('azure-user', 'azure-user'))
-  st.text(x.text)
-  
-#   subprocess.call([
+#   subprocess.call([:
 #     'curl',
 #     '-X',
 #     'POST',
@@ -21,8 +17,7 @@ if st.button('Click to run the MAIN JOB'):
 #     '-k',
 #     'http://51.103.32.188/rest/v1/group/name/smensah/project/name/yelp/version/name/default/job/name/1bis-New_data_ingestion/run?environmentName=dev'
 #     ])
-
-#   args = shlex.split(cmd)
-#   process=subprocess.Popen(args, shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-#   stdout, stderr = process.communicate()
+  args = shlex.split(cmd)
+  process=subprocess.Popen(args, shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+  stdout, stderr = process.communicate()
 
