269b5e6210e01fb20d99cb72a993ee947219bee7
MENSAH Steeve <74554422+Steven144444@users.noreply.github.com>
2023-03-13 10:11:59 +0100

    Update Second_page.py

--- pages/Second_page.py
+++ pages/Second_page.py
@@ -9,9 +9,11 @@ st.title("Test curl command")
 matillion = 'curl -X POST -u azure-user:azure-user -k http://51.103.32.188/rest/v1/group/name/smensah/project/name/yelp/version/name/default/job/name/1bis-New_data_ingestion/run?environmentName=dev'
 cmd = '''curl -X POST -u azure-user:azure-user -k http://51.103.32.188/rest/v1/group/name/smensah/project/name/yelp/version/name/default/job/name/1bis-New_data_ingestion/run?environmentName=dev'''
 
+c = webbrowser.get('firefox')
+
 if st.button('Click to run the FIRST MAIN JOB'):
   #requests.post("http://51.103.32.188/rest/v1/group/name/smensah/project/name/yelp/version/name/default/job/name/1bis-New_data_ingestion/run?environmentName=dev")
-  webbrowser.open_new_tab('https://documentation.matillion.com/docs/')
+  c.open_new_tab('https://documentation.matillion.com/docs/')
 #   subprocess.call([
 #     'curl',
 #     '-X',
